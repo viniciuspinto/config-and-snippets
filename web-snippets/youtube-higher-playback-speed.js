2cec196a8dfae9708e9316f6a77de3b42136fd7a
Vinicius Pinto <viniciusweb@gmail.com>
2015-10-12 16:56:16 -0300

    YouTube playback speed JS snippet

---
+++ web-snippets/youtube-higher-playback-speed.js
@@ -0,0 +1,4 @@
+// YouTube video player only allows setting playback speed up to 2x
+// This snipped can be used to set higher values
+vid = document.getElementsByClassName("video-stream html5-main-video")[0];
+vid.playbackRate = 3.0;
